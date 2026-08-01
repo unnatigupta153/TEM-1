5d55a547e93b6c0ee6e829f5926786d7ecf032bd
unnatigupta153 <guptaunnati913@gmail.com>
2026-08-01 17:13:18 +0530

    Create Tasks.jsx

---
+++ client/src/pages/Tasks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TaskPage from '../components/TaskPage'
+import WorkLogPage from '../components/WorkLogPage'
+
+const Tasks = () => {
+  return (
+    <>
+      <TaskPage/>
+      <WorkLogPage/>
+    </>
+  )
+}
+
+export default Tasks
